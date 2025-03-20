89c88903e2e6692cf674e42ea852842a682fab97
dieto01 <diego.silva1225@etec.sp.gov.br>
2025-03-20 01:50:24 +0000

    teste

--- app/index.php
+++ app/index.php
@@ -1,8 +1,47 @@
 <?php
-// require './vendor/autoload.php';
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request; // Corrigido
 
-// use Application\DBConnection\MySQLConnection;
+use Slim\Factory\AppFactory;
 
-// $db = new MySQLConnection();
 
-echo phpinfo();
+use Slim\Views\PhpRenderer; // Corrigido
+
+require __DIR__ . '/vendor/autoload.php';
+
+$app = AppFactory::create();
+
+$app->get('/',
+ function(Request $request, Response $response, $args) {
+    $response->getBody()->write("ola mundo!!!");
+    return $response;
+ }
+);
+
+$app->get('/mensagem/{nome}',
+ function(Request $request, Response $response, $args) { 
+    $response->getBody()->write("boa noite ");
+    return $response;
+ });
+
+$app->get('/hello',
+ function(Request $request, Response $response, $args) { 
+    $renderer = new PhpRenderer(__DIR__ . '/templates');
+    return $renderer->render($response, 'hello.php', [mensagem]);
+ });
+
+
+$app->run();
+
+
+
+
+
+
+
+
+
+
+
+
+
